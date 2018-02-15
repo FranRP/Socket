0507b7101afb25163d062024545394ba46b718e8
FranRP <kirtashblog@gmail.com>
2018-02-15 13:42:43 +0100

    Heroku

--- dados/server/index.js
+++ dados/server/index.js
@@ -1,28 +1,13 @@
-// let express = require('express')
-// let app = express();
-//
-// let http = require('http');
-// let server = http.Server(app);
-//
-// let socketIO = require('socket.io');
-// let io = socketIO(server);
-//
-// const port = process.env.PORT || 3000;
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketIO = require('socket.io');
+let express = require('express')
+let app = express();
 
+let http = require('http');
+let server = http.Server(app);
 
-const publicPath = path.join(__dirname, '../public');
+let socketIO = require('socket.io');
+let io = socketIO(server);
+
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
-
-app.use(express.static(publicPath));
-
-
 var users = [];
 let roomno = 1;
 var salaUsers = [[]];
